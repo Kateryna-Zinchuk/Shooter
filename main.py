106957bf59184cb7600fc34815229a8ee3a7c7f9
Kateryna Zinchuk <katyazin4uk@gmail.com>
2023-01-28 10:02:52 +0100

    add class Player

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ fon = pygame.image.load(file_path("galaxy.jpg"))
 fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
 
 pygame.mixer.music.load(file_path("main.wav"))
-pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.set_volume(0.4)
 pygame.mixer.music.play(-1)
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -25,7 +25,7 @@ class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image, x, y, width, height, speed):
         super().__init__()
         self.image = pygame.image.load(file_path(image))
-        self.image = pygame.transform.scale(self.image, (wigth, height))
+        self.image = pygame.transform.scale(self.image, (width, height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -33,6 +33,23 @@ class GameSprite(pygame.sprite.Sprite):
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def __init__(self, image, x, y, width, height, speed):
+        super().__init__(image, x, y, width, height, speed)
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.x += self.speed
+
+    def fire(self):
+        pass
+
+
+player = Player("rocket.png", 300, 400, 70, 70, 5)
 
 game = True
 play = True
@@ -44,6 +61,8 @@ while game == True:
 
     window.blit(fon, (0, 0))
 
+    player.reset()
+    player.update()
 
     clock.tick(FPS)        
     pygame.display.update()
